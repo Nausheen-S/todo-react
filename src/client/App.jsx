0c1bef86ce92e347b11e4c1fd26924498bb3d15b
Nausheen_S <salauddinnausheen@gmail.com>
2020-06-01 19:30:22 +0800

    completed MVP and length validation further

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,63 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Welcome.
-      </div>
-    );
-  }
+
+    constructor(){
+      super()
+      this.state = {
+        list:"",
+        completeList : []
+      }
+      //this.handleChange = this.handleChange.bind(this);
+      //this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event){
+      this.setState({
+        list:event.target.value
+      });
+    }
+    handleSubmit(event){
+        //var list=event.target.value
+      //this.setState({
+      //  completeList:[list, ...this.state.completeList],
+       // list:""
+
+      //});
+      var newList=this.state.list;
+    if(newList.length<3||newList.length>200){
+        console.log("if works");
+        this.setState({
+            //list:"length mismatch",
+            inputClass:"banana"
+    })
+    }else{
+        var newList=this.state.completeList;
+        newList.push(this.state.list);
+        this.setState({list:""});
+
+      //event.preventDefault();
+    }
+    }
+
+    render() {
+        console.log("rendering");
+
+        var completeList = this.state.completeList.map(completeList=>{
+            return(<li>{completeList}</li>)
+        })
+        return (
+          <div className="item">
+            <button onClick={(event)=>{this.handleSubmit()}}>Add to list</button>
+            <ul>{completeList}</ul>
+            <input className={this.state.inputClass}  onChange={(event)=>{this.handleChange(event); }} value ={this.state.list}/>
+
+          </div>
+        );
+    }
+
 }
 
 export default hot(module)(App);
